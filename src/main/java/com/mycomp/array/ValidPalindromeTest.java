d6e9a75b5fd4b707c1139c17ed9670890d24166f
Pradyumna <tech.daspk@gmail.com>
2024-02-11 12:22:15 +0000

    Test

--- src/main/java/com/mycomp/array/ValidPalindromeTest.java
+++ src/main/java/com/mycomp/array/ValidPalindromeTest.java
@@ -3,10 +3,12 @@ package com.mycomp.array;
 
 public class ValidPalindromeTest {
     public static void main(String[] args) {
-        String s = "0aba0";
+        String s = ".***0aba0.*?.....";
+
         Palindrome palindrome = new Palindrome();
         System.out.println(palindrome.isPalindrome(s));
         System.out.println(palindrome.isPalindrome1(s));
+        System.out.println(palindrome.isPalindromeUsingRec(s));
     }
 }
 
@@ -16,6 +18,24 @@ class Palindrome{
         StringBuffer buffer = new StringBuffer(str);
         String reverse = buffer.reverse().toString();
         return reverse.equals(str);
+    }
+
+    public boolean isPalindromeUsingRec(String s){
+        int l=0;
+        int r= s.length()-1;
+        return isPalindromeUsingRecursion(s,l,r);
+    }
+
+    public boolean isPalindromeUsingRecursion(String s,int l,int r){
+        if(l>=r)
+            return true;
+        if(!Character.isLetterOrDigit(s.charAt(l)))
+           return  isPalindromeUsingRecursion(s,l+1,r);
+        if(!Character.isLetterOrDigit(s.charAt(r)))
+           return isPalindromeUsingRecursion(s,l,r-1);
+        if(s.charAt(l)!=s.charAt(r))
+            return false;
+        return isPalindromeUsingRecursion(s,l+1,r-1);
     }
 
     public String convertStringToValidAlphaNumeric(String s){
